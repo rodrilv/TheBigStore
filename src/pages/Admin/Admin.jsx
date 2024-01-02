b827dbae1aa20d61265f20d0062b869e240bfad2
Rodrigo Sanchez Garcia <rodrigo.sanchez@cwvlconsultoria.com>
2024-01-02 17:27:46 -0600

    hotfix

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,11 +1,23 @@
 import { Navigate } from "react-router-dom";
 import { Sidebar } from "../../components";
 import "./Admin.css";
+import { useState } from "react";
 
 export const Admin = ({ children }) => {
+  const [segment, setSegment] = useState(0);
+
   return (
-    <div>
-      <Sidebar />
+    <div style={{ display: "grid", gridTemplateColumns: "300px 1200px" }}>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <div>
+          <p>Subir un videojuego</p>
+          <p>Subir una Consola</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
