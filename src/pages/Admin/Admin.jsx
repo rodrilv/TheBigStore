da1d375a9b62aade5897e2607dd921e3e2f0dfb6
rodrilv <rott.9954@gmail.com>
2024-01-02 22:58:56 -0600

    feat

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom";
 import { Sidebar } from "../../components";
 import "./Admin.css";
 import { useState } from "react";
+import "animate.css";
 
 export const Admin = ({ children }) => {
   const [segment, setSegment] = useState(0);
@@ -11,12 +12,17 @@ export const Admin = ({ children }) => {
       <div>
         <Sidebar />
       </div>
-      <div>
-        <div>
-          <p>Subir un videojuego</p>
-          <p>Subir una Consola</p>
+      <div className="segment">
+        <div onClick={() => setSegment(0)} className="segment-1">
+          <p className={segment === 0 ? "selected-segment" : ""}>
+            Subir un Videojuego
+          </p>
         </div>
-        <div></div>
+        <div onClick={() => setSegment(1)} className="segment-2">
+          <p className={segment === 1 ? "selected-segment" : ""}>
+            Subir una Consola
+          </p>
+        </div>
       </div>
     </div>
   );
